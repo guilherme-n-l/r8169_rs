d71569feb106dcfc4d59a47ce5ecb6484ef4b07a
guilherme-n-l <acc.guilhermenl@gmail.com>
2025-09-14 18:47:39 -0300

    feat: initial commit

---
+++ r8169_rs.rs
@@ -0,0 +1,11 @@
+use kernel::pci;
+
+struct Rtl8169Driver;
+
+kernel::module_pci_driver! {
+    type: Rtl8169Driver,
+    name: "r8169_rs",
+    authors: ["Guilherme Lima"],
+    description: "RealTek 8169/8168/8101 ethernet driver rust port.",
+    license: "GPL v2",
+}
