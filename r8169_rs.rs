0caa6e9f6c443a8c2349be1eee1f55c48bec03c2
Guilherme Novaes Lima <guilh@Guilhermes-MacBook-Air.local>
2025-09-19 22:51:04 -0300

    WIP: driver implementation

--- r8169_rs.rs
+++ r8169_rs.rs
@@ -3,6 +3,10 @@ mod defs;
 use crate::defs::*;
 use kernel::pci;
 
+impl pci::Driver for Rtl8169Driver {
+    const ID_TABLE: pci::IdTable<Self::IdInfo> = &RTL8169_PCI_TABLE;
+}
+
 kernel::module_pci_driver! {
     type: Rtl8169Driver,
     name: "r8169_rs",
